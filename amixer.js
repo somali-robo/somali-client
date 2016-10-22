b97e7b8e687655a0a706f5ac0944ac7d807043d4
古川信行 <t_sakura@m1.ffn.ne.jp>
2016-10-22 16:49:37 +0900

    amixer.js 修正

--- amixer.js
+++ amixer.js
@@ -6,7 +6,7 @@ Amixer.prototype.exec = require('child_process').exec;
 //再生 音量設定
 Amixer.prototype.pcmVolume = function(volume,callback){
   var cmd = 'amixer set PCM '+volume+'%';
-  console.log('volume '+cmd);
+  console.log('pcmVolume '+cmd);
   this.exec(cmd, function(err, stdout, stderr){
     callback(err, stdout, stderr);
   });
@@ -15,7 +15,7 @@ Amixer.prototype.pcmVolume = function(volume,callback){
 //録音音量 設定
 Amixer.prototype.micVolume = function(volume,callback){
   var cmd = 'amixer sset Mic '+volume+'%';
-  console.log('volume '+cmd);
+  console.log('micVolume '+cmd);
   this.exec(cmd, function(err, stdout, stderr){
     callback(err, stdout, stderr);
   });
