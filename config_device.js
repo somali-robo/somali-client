aaf3b00da79388a4e27db9473e204e1509cb087d
古川信行 <t_sakura@m1.ffn.ne.jp>
2016-10-22 16:31:03 +0900

    config_device.jsを追加

---
+++ config_device.js
@@ -0,0 +1,27 @@
+/** デバイスのピン設定
+*
+*/
+var ConfigDevice = function(){};
+
+//WPS ボタン (水色)
+ConfigDevice.prototype.WPS_BUTTON = 22;
+
+//REC ボタン (赤色)
+ConfigDevice.prototype.REC_BUTTON = 23;
+
+//モード切り替え
+ConfigDevice.prototype.MODE_SWITCH = 24;
+
+//スピーカー・アンプ
+ConfigDevice.prototype.SPEAKER_AMP_POWER = 25;
+
+//Voice Magic i2c,0x2B
+ConfigDevice.prototype.VOICE_MAGIC_I2C_ADDR  = 0x2B;
+ConfigDevice.prototype.VOICE_MAGIC_PSV_N = 18;
+ConfigDevice.prototype.VOICE_MAGIC_INT   = 17;
+ConfigDevice.prototype.VOICE_MAGIC_VCST  = 27;
+
+//MPU6050 i2c
+ConfigDevice.prototype.MPU6050_I2C_ADDR  = 0x68;
+
+module.exports = new ConfigDevice();
--- test/test_device.js
+++ test/test_device.js
@@ -1,7 +1,7 @@
 
 var App = function(){};
 App.prototype.wpi    = require('wiring-pi');
-App.prototype.config = require('../config.js');
+App.prototype.configDevice = require('../config_device.js');
 App.prototype.aplay  = require('../aplay.js');
 App.prototype.voiceMagic  = require('../voice_magic.js');
 
@@ -10,24 +10,25 @@ App.prototype.init = function(){
   console.log("init");
 
   var _this = this;
+
   //GPIO初期化
   this.wpi.wiringPiSetupGpio();
 
   //スピーカー・アンプ
-  this.wpi.pinMode(this.config.SPEAKER_AMP_POWER,this.wpi.OUTPUT);
+  this.wpi.pinMode(this.configDevice.SPEAKER_AMP_POWER,this.wpi.OUTPUT);
   this.speakerAmpPower(false);
 
   //WPS ボタン（青） INT_EDGE_RISING 立ち上がる時
-  this.wpi.pinMode(this.config.WPS_BUTTON,this.wpi.INPUT);
-  this.wpi.wiringPiISR(this.config.WPS_BUTTON, this.wpi.INT_EDGE_RISING, function(delta) {
+  this.wpi.pinMode(this.configDevice.WPS_BUTTON,this.wpi.INPUT);
+  this.wpi.wiringPiISR(this.configDevice.WPS_BUTTON, this.wpi.INT_EDGE_RISING, function(delta) {
     console.log("WPS_BUTTON " + delta);
     //アンプをOFFにする
     _this.speakerAmpPower(false);
   });
 
   //REC ボタン(赤) INT_EDGE_RISING 立ち上がる時
-  this.wpi.pinMode(this.config.REC_BUTTON,this.wpi.INPUT);
-  this.wpi.wiringPiISR(this.config.REC_BUTTON, this.wpi.INT_EDGE_RISING, function(delta) {
+  this.wpi.pinMode(this.configDevice.REC_BUTTON,this.wpi.INPUT);
+  this.wpi.wiringPiISR(this.configDevice.REC_BUTTON, this.wpi.INT_EDGE_RISING, function(delta) {
     console.log("REC_BUTTON " + delta);
 
     //アンプをONにする
@@ -45,8 +46,8 @@ App.prototype.init = function(){
   });
 
   //モード スイッチ INT_EDGE_BOTH 両方
-  this.wpi.pinMode(this.config.MODE_SWITCH,this.wpi.INPUT);
-  this.wpi.wiringPiISR(this.config.MODE_SWITCH, this.wpi.INT_EDGE_BOTH, function(delta) {
+  this.wpi.pinMode(this.configDevice.MODE_SWITCH,this.wpi.INPUT);
+  this.wpi.wiringPiISR(this.configDevice.MODE_SWITCH, this.wpi.INT_EDGE_BOTH, function(delta) {
     console.log("MODE_SWITCH " + delta);
   });
 };
@@ -55,11 +56,11 @@ App.prototype.init = function(){
 App.prototype.speakerAmpPower = function(isOn){
   if(isOn == true){
     //ON
-    this.wpi.digitalWrite(this.config.SPEAKER_AMP_POWER,this.wpi.HIGH);
+    this.wpi.digitalWrite(this.configDevice.SPEAKER_AMP_POWER,this.wpi.HIGH);
   }
   else{
     //OFF
-    this.wpi.digitalWrite(this.config.SPEAKER_AMP_POWER,this.wpi.LOW);
+    this.wpi.digitalWrite(this.configDevice.SPEAKER_AMP_POWER,this.wpi.LOW);
   }
 };
 
