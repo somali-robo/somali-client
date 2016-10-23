9b762770de5bdd9d181a9c582537859fb7fe034a
古川信行 <t_sakura@m1.ffn.ne.jp>
2016-10-24 01:37:14 +0900

    mpu6050.js

---
+++ mpu6050.js
@@ -0,0 +1,15 @@
+/** MPU6050
+*/
+var Mpu6050 = function(){};
+Mpu6050.prototype.mpu6050 = require('mpu6050-wiringpi');
+
+//MPU-6050 データ購読
+Mpu6050.prototype.subscribe = function(sec,callback){
+  var _this = this;
+  setInterval(function(){
+    var data = _this.mpu6050.read();
+    callback(data);
+  },sec*1000);
+};
+
+module.exports = new Mpu6050();
