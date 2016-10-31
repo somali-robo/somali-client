a677817f7468988da867b33a4e6aa3c3ccb28a13
古川信行 <t_sakura@m1.ffn.ne.jp>
2016-10-31 14:46:01 +0900

    test_socket.jsに音声合成追加

--- test/test_socket.js
+++ test/test_socket.js
@@ -4,6 +4,8 @@
 var App = function(){};
 App.prototype.config = require('../config.js');
 App.prototype.somaliSocket = require('../somali_socket.js');
+App.prototype.hoya = require('../hoya.js');
+App.prototype.aplay = require('../aplay.js');
 
 //初期化
 App.prototype.init = function(){
@@ -12,6 +14,21 @@ App.prototype.init = function(){
   this.somaliSocket.init(this.config,function(data){
     console.log('publish');
     console.log(data);
+    if(data.userId != _this.config.DEVICE_ID){
+      //スマートフォンからのメッセージなので音声合成
+      _this.textToSpeech(data.value,"bear",function(path, err){
+        if (err != null){
+          console.log("err");
+          return;
+        }
+        console.log("success");
+
+        //ここで再生
+        _this.aplay.play(path,function(err, stdout, stderr){
+
+        });
+      });
+    }
   });
 
   setInterval(function(){
@@ -20,6 +37,23 @@ App.prototype.init = function(){
     //メッセージ送信
     _this.somaliSocket.publish(value);
   }, 5000);
+};
+
+App.prototype.textToSpeech = function(text,speaker,callback){
+  var apiKey = this.config.DOCOMO_API_KEY;
+  var params = {};
+  var callbackTextToSpeech = function( err, resp, body ){
+    if(!err && resp.statusCode === 200){
+      //ファイル書き出し
+      var path = './tmp/textToSpeech.wav';
+      var fs = require('fs');
+      fs.writeFile(path, body, 'binary', function(err){
+          callback(path, err);
+      });
+    }
+  };
+
+  this.hoya.textToSpeech(apiKey,text,speaker,params,callbackTextToSpeech);
 };
 
 var app = new App();
