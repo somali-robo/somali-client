ac0de5097143b6302c2c88ccd58601bde7b2b3a1
古川信行 <t_sakura@m1.ffn.ne.jp>
2016-11-24 13:06:12 +0900

    store2 ライブラリ追加

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var App = function(){};
 App.prototype.uuid = require('node-uuid');
 App.prototype.dropbox = require("node-dropbox");
 App.prototype.wpi = require('wiring-pi');
-App.prototype.store = require('store2');
+App.prototype.Store = require('store2');
+App.prototype.store = null;
 
 App.prototype.config = require('./config.js');
 App.prototype.configDevice = require('./config_device.js');
@@ -82,8 +83,8 @@ App.prototype.setStatus = function(status){
 App.prototype.init = function(){
   console.log("init");
   var _this = this;
-
-  this.store(this.KEY_STORE);
+  
+  this.store = this.Store(this.KEY_STORE);
 
   //GPIO初期化
   this.wpi.wiringPiSetupGpio();
