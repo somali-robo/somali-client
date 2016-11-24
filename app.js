fa11dfcac3a5b0d757b6769ec5d0bdccf432747d
古川信行 <t_sakura@m1.ffn.ne.jp>
2016-11-24 23:51:44 +0900

    加速度センサ処理追加

--- app.js
+++ app.js
@@ -509,9 +509,10 @@ App.prototype.accelerationStart = function(){
       accelSum.accelY += data.accelY;
       accelSum.accelZ += data.accelZ;
       accelSum.cnt += 1;
+      console.log("accelSum.cnt "+accelSum.cnt);
 
       //平均を計算
-      if(accelSum.cnt > 10){
+      if(accelSum.cnt == 10){
         console.log("isShaken");
         console.log((accelSum.isShaken())?"true":"false");
         accelSum.clear();
