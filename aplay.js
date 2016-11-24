7683aabbbbf3f302f8c60454d7697d78b6b60dc4
古川信行 <t_sakura@m1.ffn.ne.jp>
2016-11-25 00:14:36 +0900

    加速度センサ処理追加

--- aplay.js
+++ aplay.js
@@ -2,12 +2,16 @@
 */
 var Aplay = function(){};
 Aplay.prototype.exec = require('child_process').exec;
+Aplay.prototype.isPlay = false;
 
 // 再生
 Aplay.prototype.play = function(path,callback){
+  if(this.isPlay == true) return;
+  var _this = this;
   var cmd = 'aplay -D plughw:1,0 '+path;
   console.log('play '+cmd);
   this.exec(cmd, function(err, stdout, stderr){
+    _this.isPlay = false;
     if(callback){
       callback(err, stdout, stderr);
     }
