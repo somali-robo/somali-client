df74b93624697f7e4acffe527808d2dbccd0c15c
古川信行 <t_sakura@m1.ffn.ne.jp>
2016-12-11 16:59:32 +0900

    チャットルームAPI修正

--- somali_api.js
+++ somali_api.js
@@ -84,7 +84,7 @@ SomaliApi.prototype.postChatRoom = function(name,members,messages,callback){
   var options = {url: this.API_HOST+this.API_CHAT_ROOMS,form: {"name":name,"members":members,"messages":messages}};
   console.log("postChatRoom");
   console.log(options);
-  console.log(options["members"]);
+  console.log(members);
   this.request.post(options,function(err,response){
     if(err){
       callback(err);
