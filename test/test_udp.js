86e980f37a94127ee406d44b2058537dc7e6947a
古川信行 <t_sakura@m1.ffn.ne.jp>
2016-12-17 16:23:24 +0900

    UDPライブラリ追加

--- test/test_udp.js
+++ test/test_udp.js
@@ -10,8 +10,6 @@ App.prototype.init = function(){
 
   //初期化
   this.serverDgram.init();
-  this.clientDgram.init();
-
   //受信設定
   this.serverDgram.bind(function(msg, rinfo){
     console.log(rinfo);
@@ -20,11 +18,12 @@ App.prototype.init = function(){
     //console.log('data len: '+ rinfo.size + " data: "+msg.toString('ascii', 0, rinfo.size));
   });
 
+  this.clientDgram.init();
   //定期的にメッセージを送信してみる
   const message = new Buffer("nantekottai.");
-  this.clientDgram.send(message,this.clientDgram.UDP_PORT,'255.255.255.255');
+  this.clientDgram.send(message,this.clientDgram.UDP_PORT,'127.0.0.1');
   setInterval(function(){
-    _this.clientDgram.send(message,_this.clientDgram.UDP_PORT,'255.255.255.255');
+    _this.clientDgram.send(message,_this.clientDgram.UDP_PORT,'127.0.0.1');
   },5*1000);
 };
 
