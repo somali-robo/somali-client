474813e6d3f6c2f14d3afdb75b3a6d9ac85081a0
古川信行 <t_sakura@m1.ffn.ne.jp>
2017-01-04 00:53:17 +0900

    エラーメッセージ修正

--- app.js
+++ app.js
@@ -96,10 +96,13 @@ App.prototype.helpWavFilePath = "./resources/help.wav";
 //error発生時の処理
 App.prototype.onError = function(){
   const _this = this;
-  if((this.oldLastErr != null)&&(this.oldLastErr === this.lastErr)){
+  if((this.oldLastErr != null)&&(this.oldLastErr == this.lastErr)){
     return;
   }
+  console.log("----");
   console.log(this.lastErr);
+  console.log("----");
+  console.log(this.oldLastErr);
   try{
     console.log("play start.");
     this.wavPlay(this.errWavFilePath);
