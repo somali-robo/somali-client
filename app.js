8518e54394b3a6beb47fd4d04602735cc59337dc
古川信行 <t_sakura@m1.ffn.ne.jp>
2017-01-16 20:10:28 +0900

    ペアリング処理変更

--- app.js
+++ app.js
@@ -1174,18 +1174,16 @@ App.prototype.creteGroupChatRoom = function(joinSerialCode,isSpeech){
   if(roomId != null){
     console.log("creteGroupChatRoom roomId is not null");
     
-    if(!isSpeech){
-      const msg = "友達が遊びに来たよ";
-      _this.textToSpeech(msg,_this.SPEAKER_TYPE,function(path, err){
-        if (err != null){
-          console.log("err");
-          return;
-        }
-        console.log("success");
-        _this.wavPlay(path,function(code,err){
-        });
+    const msg = "友達が遊びに来たよ";
+    _this.textToSpeech(msg,_this.SPEAKER_TYPE,function(path, err){
+      if (err != null){
+        console.log("err");
+        return;
+      }
+      console.log("success");
+      _this.wavPlay(path,function(code,err){
       });
-    }
+    });
 
     //既に作成済みなので ルームID を broadcastして通知する
     const code = _this.config.SERIAL_CODE;
