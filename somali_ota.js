adef96ac17c7f2af6924d2396f788560d08d8f6e
古川信行 <t_sakura@m1.ffn.ne.jp>
2017-05-15 16:59:54 +0900

    otaコード修正

--- somali_ota.js
+++ somali_ota.js
@@ -92,7 +92,7 @@ SomaliOta.prototype.start = function(callback){
               }
 
               // mv somali-client-master/* .
-              _this.exec('cp',['-rf',destPath+'/somali-client-master/*','.'],function(code,err){
+              _this.exec('cp',['-rfT',destPath+'/somali-client-master/','.'],function(code,err){
                 if(err){
                   //OTA 何らかのエラー
                   callback(null,err);
